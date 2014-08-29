127fcd225aceb9163c447232890e25c712a10bc7
LEmax <lemaxe@gmail.com>
2014-08-30 00:53:34 +0400

    CurrentDir, include config

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
+define("CURRENT_WORKING_DIR", str_replace("\\", "/", $dirname = dirname(__FILE__)));
+require CURRENT_WORKING_DIR . '/system/config.php';
+// auth identifier
+// current path identifier
 echo 'Beginning';
 ?>
